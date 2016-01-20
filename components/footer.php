3192f9dc812c0226437f2a8d5eae4e3947cfd130
Jeffrey <jeffrey@accentinteractive.nl>
2016-01-20 12:14:24 +0100

    some lovely styling for WiFi page

--- components/footer.php
+++ components/footer.php
@@ -1,6 +1,19 @@
 <footer>
     KPN Experiabox <?php echo date('Y'); ?> <?php if( is_landing_page() != true){echo '&copy; | <a href="'.$url.'help/">help</a> | <a href="'.$url.'help/">sitemap</a>';}?>
 </footer>
+
+<script>
+    $(function() {
+        $( ".groupd-accordion section a" ).click(function() {
+            if ($( this ).parent().hasClass( "selected" )){
+                $(this ).parent().removeClass("selected");
+            } else {
+                $(this).parent().addClass("selected");
+            }
+        });
+    });
+</script>
+
 <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
 <!-- Include all compiled plugins (below), or include individual files as needed -->
--- wifi.php
+++ wifi.php
@@ -12,114 +12,110 @@
                     <a href=#basis id="basis">
                         Basis
                     </a>
+
                     <div class="groupd-accordion-section">
-                        Aanzetten draadloos netwerk <input type="checkbox" name="enable" value="empty" checked><br />
-                        <label for="inputEmail"> Netwerknaam(SSID) </label>
-                        <input type="email" id="inputEmail" value="H368N4AA182" class="form-control col-md-1.5">(1 ~ 32 tekens)<br />
-                        Netwerk wachtwoord <input type="email" id="inputEmail" class="form-control col-md-1.5" value="mijnwachtwoord"><br />
+                        <h2>Wi-Fi instellingen</h2>
+
+                        <label for="networkCheck">Aanzetten draadloos netwerk</label>
+                        <br />
+                        <input type="checkbox" id="networkCheck" name="enable" checked>
+                        <br />
+
+                        <label for="networkName"> Netwerknaam (SSID) <br /> <sup>(1 ~ 32 tekens)</sup> </label>
+                        <input type="text" id="networkName" value="H368N4AA182" class="form-control col-md-1.5">
+
+                        <label for="networkPassword">Netwerk wachtwoord</label>
+                        <input type="text" id="networkPassword" class="form-control col-md-1.5" value="mijnwachtwoord"><br />
                     </div>
+
                 </section>
                 <section>
                     <a href=#geavanceerd id="geavanceerd">
                         Geavanceerd
                     </a>
                     <div class="groupd-accordion-section">
-                        <h7>Geavanceerde wachtwoord instellingen</h7><br /><br />
-                        Authenticatie Type <?php info("AuthenticatieType"); ?>
+                        <h2>Geavanceerde wachtwoord instellingen</h2>
 
-                        <select>
+                        <label for="authenticationType">Authenticatie Type <?php info("AuthenticatieType"); ?></label>
+                        <select id="authenticationType">
                             <option>WPA-PSK</option>
                             <option>WPA2-PSK</option>
                             <option selected>WPA/WPA2-PSK</option>
                             <option>WEB</option>
                         </select><br />
 
-                        WPA Encryptie Algoritme <?php info("WPAEncriptyAlgoritme"); ?>
-                        <select>
+                        <label for="encryptionAlgo">WPA Encryptie Algoritme <?php info("WPAEncriptyAlgoritme"); ?></label>
+                        <select id="encryptionAlgo">
                             <option selected>TKIP + AES</option>
                             <option>TKIP</option>
                             <option>AES</option>
                         </select><br />
 
-                    <h5>Geavanceerde Wi-Fi Instellingen</h5>
+                        <h2>Geavanceerde Wi-Fi Instellingen</h2>
+                        <label for="modus">Modus <?php info("Modus"); ?></label>
+                        <select id="modus">
+                            <option >IEEE 802.11b Only</option>
+                            <option >IEEE 802.11g Only</option>
+                            <option >IEEE 802.11n Only</option>
+                            <option >Mixed(802.11b+802.11a)</option>
+                            <option selected>Mixed(802.11b+802.11a+802.11n)</option>
+                        </select><br />
 
-                    Modus <?php info("Modus"); ?>
-                    <select>
-                        <option >IEEE 802.11b Only</option>
-                        <option >IEEE 802.11g Only</option>
-                        <option >IEEE 802.11n Only</option>
-                        <option >Mixed(802.11b+802.11a)</option>
-                        <option selected>Mixed(802.11b+802.11a+802.11n)</option>
+                        <label for="bandwidth">Kanaal bandbreedte <?php info("KanaalBandbreedte"); ?></label>
+                        <select id="bandwidth">
+                            <option selected>20MHz</option>
+                            <option>40MHz</option>
+                        </select><br />
 
-                    </select><br />
+                        <label for="channel">Kanaal</label>
+                        <select id="channel">
+                            <option selected>Auto</option>
+                            <option>1</option>
+                            <option>2</option>
+                            <option>3</option>
+                            <option>4</option>
+                            <option>5</option>
+                            <option>6</option>
+                            <option>7</option>
+                            <option>8</option>
+                            <option>9</option>
+                            <option>10</option>
+                            <option>11</option>
+                            <option>12</option>
+                            <option>13</option>
+                        </select><br />
 
-                    Kanaal bandbreedte <?php info("KanaalBandbreedte"); ?> <select>
-                        <option selected>20MHz</option>
-                        <option>40MHz</option>
-                    </select><br />
+                        <label for="signalStrength">Uitzendende Kracht</label>
+                        <select id="signalStrength">
+                            <option selected>100%</option>
+                            <option>80%</option>
+                            <option>60%</option>
+                            <option>40%</option>
+                            <option>20%</option>
+                        </select><br />
 
-                    Kanaal <select>
-                        <option selected>Auto</option>
-                        <option>1</option>
-                        <option>2</option>
-                        <option>3</option>
-                        <option>4</option>
-                        <option>5</option>
-                        <option>6</option>
-                        <option>7</option>
-                        <option>8</option>
-                        <option>9</option>
-                        <option>10</option>
-                        <option>11</option>
-                        <option>12</option>
-                        <option>13</option>
-                    </select><br />
+                        <label for="checkWMMQoS">Aanzetten WMM QoS <?php info("WMMQoS"); ?></label>
+                        <br />
+                        <input type="checkbox" name="enable" id="checkWMMQoS" value="empty">
+                        <br />
 
-                    Uitzendende Kracht <select>
-                        <option selected>100%</option>
-                        <option>80%</option>
-                        <option>60%</option>
-                        <option>40%</option>
-                        <option>20%</option>
-                    </select><br />
+                        <label for="hideWifi">Wi-Fi netwerk verbergen</label>
+                        <br />
+                        <input id="hideWifi" type="checkbox" name="enable" value="empty">
+                        <br />
 
-                    Aanzetten WMM QoS <?php info("WMMQoS"); ?>
-                    <input type="checkbox" name="enable" value="empty"><br />
+                        <label for="maxUserWifi">Maximaal aantal verbonden gebruikers<br /><sup>(1 ~ 32)</sup></label>
+                        <input id="maxUserWifi" type="number" min="1" max="32" class="form-control, col-md-1, ">
 
-                    Wi-Fi netwerk verbergen <input type="checkbox" name="enable" value="empty"><br />
-                    Maximaal aantal verbonden gebruikers <input type="number" min="1" max="32" class="form-control, col-md-1, "> (1 ~ 32)<br/>
+                        <h2>KPN FON Hotspot</h2>
 
-                    <h7>KPN FON Hotspot</h7><br />
-                    Enable KPN-FON <?php info("KPN-FON"); ?> <input type="checkbox" checked>
-                    <//div>
+                        <label for="turnKPNFON">Schakel KPN-FON in <?php info("KPN-FON"); ?></label>
+                        <br />
+                        <input id="turnKPNFON" type="checkbox" checked>
+                        <br />
+                    </div>
                 </section>
             </div>
-
-            <script>
-                $('#fold').css("display", 'none');
-                $(function() {
-                    $( ".groupd-accordion section a" ).click(function() {
-                        if ($( this ).parent().hasClass( "selected" )){
-                            $(this ).parent().removeClass("selected");
-                        } else {
-                            $(this).parent().addClass("selected");
-                        }
-                    });
-                });
-
-
-//                function unfold(){
-//                    $( ".shortBriefCode" ).addClass("selected");
-//                    $("#unfold").css("display", 'none');
-//                    $('#fold').css("display", 'block');
-//                }
-//
-//                function fold(){
-//                    $( ".shortBriefCode" ).removeClass("selected");
-//                    $('#fold').css("display", 'none');
-//                    $("#unfold").css("display", 'block');
-//                }
-            </script>
         </div>
     </div>
 </div>
